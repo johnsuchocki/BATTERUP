baa2aaa3d5a014aea61d9aec16e2918bdcbcc442
John <jsuchocki@gmail.com>
2016-08-10 11:26:43 -0400

    updated user entry validation

--- BattingAvgSlugCalc.java
+++ BattingAvgSlugCalc.java
@@ -13,6 +13,8 @@ public class BattingAvgSlugCalc {
 		String numOfBatters = userInput.nextLine();
 		int numBatters = Integer.parseInt(numOfBatters);
 		
+		Batters[] batterData = new Batters[numBatters];
+		
 		for (int j = 0; j < numBatters; j++) {
 	
 			System.out.println("What is the name of batter #" + (j + 1) + "?");
@@ -30,10 +32,24 @@ public class BattingAvgSlugCalc {
 			for (int i = 0; i < atBats; i++) {
 				System.out.println("What happened during at bat # " + (i + 1) + "?");
 				String currentBat = userInput.nextLine();
-				batterHits[i] = Integer.parseInt(currentBat);
+				
+				//Validates user entry for hit types
+				//if user entry is correct, the value is plugged into the array
+				int hitValidCheck = Integer.parseInt(currentBat);
+				if ((hitValidCheck == 0) || (hitValidCheck == 1) || (hitValidCheck == 2) || 
+						(hitValidCheck == 3) || (hitValidCheck == 4)) {
+					batterHits[i] = hitValidCheck;
+				} else {
+				//if user entry is incorrect, this iteration of the loop is repeated
+				//until the user entry is correct
+					System.out.println("That entry is invalid");
+					i--;
+				}	
 			}
 		
 			Batters batter = new Batters(playerName, atBats, batterHits);
+			batterData[j] = batter;
+			
 			System.out.print(playerName + "\'s batting average is ");
 			System.out.printf("%.3f", batter.getBatAvg());
 			System.out.println("");
